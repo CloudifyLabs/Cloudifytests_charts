cbb6933bbc41107bda998782e3605859b8ae0392
Roshanitft <99382098+Roshanitft@users.noreply.github.com>
2023-07-10 14:47:41 +0530

    Update gcp.sh

--- gcp.sh
+++ gcp.sh
@@ -29,4 +29,81 @@ curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/d
 sudo mv /tmp/eksctl /usr/local/bin
 fi
 
-az login
+
+#Cluster details.
+
+read -p "Enter Yes to create cluster  or Enter No if you already have a GKE cluster : " flag
+if [[ $flag == "yes" || $flag == "Yes" ]]; then
+   echo -e "\nEnter your cluster details\n"
+
+   
+
+   read -p "Enter the name of the cluster (default name - cloudifytests): " CLUSTER_NAME
+   if [[ -z $CLUSTER_NAME ]]
+   then
+    CLUSTER_NAME=cloudifytests
+   fi
+   echo -e "\nYour Cluster Name will be : $CLUSTER_NAME\n"
+
+
+   read -p "Enter your gcloud region where you want to create your cluster (default gcloud region - us-central1 ): " ZONE 
+   if [[ -z $ZONE ]]
+   then
+    ZONE=us-central1
+   fi
+   echo -e "\nYour gcloud region will be : $ZONE\n"
+
+   read -p "Enter your gcloud 1st node-pool name (default node-pool name - userapp ): " NODE_POOL_1
+   if [[ -z $NODE_POOL_1 ]]
+   then
+    NODE_POOL_1=userapp
+   fi
+   echo -e "\nYour gcloud 1st node-pool name will be : $NODE_POOL_1\n"
+
+   read -p "Enter your gcloud 1st node-pool name (default node-pool name - browsersession ): " NODE_POOL_2
+   if [[ -z $NODE_POOL_2 ]]
+   then
+    NODE_POOL_2=browsersession
+   fi
+   echo -e "\nYour gcloud 2nd node-pool name will be : $NODE_POOL_2\n"
+
+
+fi
+read -p "Enter your-project-id : " PROJECT_ID
+
+gcloud config set project $PROJECT_ID
+
+gcloud services enable container.googleapis.com
+
+
+
+# Create the cluster
+gcloud container clusters create "$CLUSTER_NAME" \
+  --project "$PROJECT_ID" \
+  --zone "$ZONE" \
+  --machine-type "$MACHINE_TYPE" \
+  --enable-autoscaling \
+  --num-nodes "$NUM_NODES_1" \
+  --node-taints userapp=true:NoSchedule
+
+# Create the second node group
+gcloud container node-pools create $NODE_POOL_2 \
+  --cluster "$CLUSTER_NAME" \
+  --project "$PROJECT_ID" \
+  --zone "$ZONE" \
+  --machine-type "$MACHINE_TYPE" \
+  --enable-autoscaling \
+  --num-nodes "$NUM_NODES_2" \
+  --node-taints browsersession=true:NoSchedule
+
+
+ kubectl patch deployment coredns -p '{"spec":{"template":{"spec":{"tolerations":[{"effect":"NoSchedule","key":"userapp","value":"true"}]}}}}' -n kube-system
+
+ #helm repo add autoscaler https://kubernetes.github.io/autoscaler
+
+ #helm install my-release autoscaler/cluster-autoscaler --set  'autoDiscovery.clusterName'=$CLUSTER_NAME  --set tolerations[0].key=userapp --set-string tolerations[0].value=true --set tolerations[0].operator=Equal --set tolerations[0].effect=NoSchedule  --set awsRegion=$aws_region2
+  
+  
+ kubectl apply -f metrics-deployment.yml  
+
+
